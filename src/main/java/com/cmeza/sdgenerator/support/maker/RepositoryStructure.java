59c97c55ef2bd1ac75f2705182cb0c39807ceae4
Carlos Meza Yana <cmeza.20@gmail.com>
2017-07-17 22:42:11 -0500

    1.1.3 -> Support @EmbeddedId, @Id (Getter method)

--- src/main/java/com/cmeza/sdgenerator/support/maker/RepositoryStructure.java
+++ src/main/java/com/cmeza/sdgenerator/support/maker/RepositoryStructure.java
@@ -4,12 +4,13 @@ import com.cmeza.sdgenerator.support.maker.builder.ObjectBuilder;
 import com.cmeza.sdgenerator.support.maker.builder.ObjectStructure;
 import com.cmeza.sdgenerator.support.maker.values.ObjectTypeValues;
 import com.cmeza.sdgenerator.support.maker.values.ScopeValues;
-import com.cmeza.sdgenerator.util.CustomResourceLoader;
-import com.cmeza.sdgenerator.util.SDLogger;
-import com.cmeza.sdgenerator.util.Tuple;
+import com.cmeza.sdgenerator.util.*;
 
+import javax.persistence.EmbeddedId;
 import javax.persistence.Id;
+import java.io.Serializable;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * Created by carlos on 08/04/17.
@@ -23,22 +24,23 @@ public class RepositoryStructure {
     public RepositoryStructure(String repositoryPackage, String entityName, String entityClass, String postfix, CustomResourceLoader loader) {
         this.loader = loader;
         String repositoryName = entityName + postfix;
-        String entityId = getEntityId(entityClass);
+        Tuple<String, Boolean> entityId = getEntityId(entityClass);
         if(entityId != null) {
             this.objectBuilder = new ObjectBuilder(
                     new ObjectStructure(repositoryPackage, ScopeValues.PUBLIC, ObjectTypeValues.INTERFACE, repositoryName)
                             .addImport(entityClass)
                             .addImport("org.springframework.data.jpa.repository.JpaRepository")
                             .addImport("org.springframework.stereotype.Repository")
+                            .addImport(entityId.right() ? entityId.left() : "")
                             .addAnnotation("Repository")
-                            .setExtend("JpaRepository", entityName, entityId)
+                            .setExtend("JpaRepository", entityName, GeneratorUtils.getSimpleClassName(entityId.left()))
             );
         }
     }
 
     @SuppressWarnings("unchecked")
-    private String getEntityId(String entityClass){
-        try{
+    private Tuple<String, Boolean> getEntityId(String entityClass){
+        try {
             Class<?> entity = null;
             if (loader == null) {
                 entity = Class.forName(entityClass);
@@ -48,20 +50,50 @@ public class RepositoryStructure {
 
             for (Field field : entity.getDeclaredFields()) {
 
-                if (field.isAnnotationPresent(Id.class)){
-                    return field.getType().getSimpleName();
+                if (field.isAnnotationPresent(Id.class) || field.isAnnotationPresent(EmbeddedId.class)) {
+                    this.implementsSerializable(field.getType());
+                    return new Tuple<>(field.getType().getName(), this.isCustomType(field.getType()));
                 }
             }
 
-            error = SDLogger.addError("Repository Error: @Id not found: " + entityClass);
+            for (Method method : entity.getDeclaredMethods()) {
+                if (!method.getReturnType().equals(Void.TYPE) && (method.isAnnotationPresent(Id.class) || method.isAnnotationPresent(EmbeddedId.class))) {
+                    this.implementsSerializable(method.getReturnType());
+                    return new Tuple<>(method.getReturnType().getName(), this.isCustomType(method.getReturnType()));
+                }
+            }
+
+            error = SDLogger.addError("Repository Error: Primary key not found in " + GeneratorUtils.getSimpleClassName(entityClass) + ".java");
             return null;
-        }catch (Exception e) {
-            error = SDLogger.addError( "Repository Error: Failed to access entity" + entityClass);
+        } catch (GeneratorException ex) {
+            error = SDLogger.addError(ex.getMessage());
+            return null;
+        } catch (Exception e) {
+            error = SDLogger.addError("Repository Error: Failed to access entity " + GeneratorUtils.getSimpleClassName(entityClass) + ".java");
             return null;
         }
     }
 
     public Tuple<String, Integer> build(){
         return new Tuple<>(objectBuilder == null ? null : objectBuilder.build(), error);
+    }
+
+    private boolean isCustomType(Class<?> clazz) {
+        if (clazz.isAssignableFrom(Boolean.class) ||
+                clazz.isAssignableFrom(Byte.class) ||
+                clazz.isAssignableFrom(String.class) ||
+                clazz.isAssignableFrom(Integer.class) ||
+                clazz.isAssignableFrom(Long.class) ||
+                clazz.isAssignableFrom(Float.class) ||
+                clazz.isAssignableFrom(Double.class)){
+            return false;
+        }
+        return true;
+    }
+
+    private void implementsSerializable(Class<?> clazz) throws GeneratorException{
+        if (!Serializable.class.isAssignableFrom(clazz)) {
+            throw new GeneratorException("Type parameter '" + clazz.getName() + "' should implement 'java.io.Serializable'");
+        }
     }
 }
---
+++ src/main/java/com/cmeza/sdgenerator/util/GeneratorException.java
@@ -0,0 +1,15 @@
+package com.cmeza.sdgenerator.util;
+
+/**
+ * Created by carlos on 17/07/17.
+ */
+public class GeneratorException extends RuntimeException{
+
+    public GeneratorException() {
+        super();
+    }
+
+    public GeneratorException(String message) {
+        super(message);
+    }
+}
